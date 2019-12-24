78037058acc17fab891336f09a38082804e26edc
dmseo1 <dkfk2747@gmail.com>
2019-12-24 17:03:40 +0900

    17:00

---
+++ lecture6/object.js
@@ -0,0 +1,32 @@
+var MyObject = {
+    //key value ---> JSON 형태!
+    "a" : 3,
+    "b" : "c",  //프로퍼티
+    "d" : () => {
+        
+    },   //메소드
+    "obj" : {
+        "e" : 1,
+        "f" : 2
+    }   //객체 안의 객체
+
+    //3개의 프로퍼티, 1개의 메소드
+}
+
+
+object.a
+object.d()
+object.obj.e
+
+var object3 = "asdfasdfadf"; //문자열을 담은 변수 -> 이것 또한 객체
+
+var object4 = new String(); //문자열을 포함하는 객체
+
+var object1 = new Object();
+
+var object5 = {}
+
+let dateObject = new Date();
+
+Date().
+dateObject.setHours()
